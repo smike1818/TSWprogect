023aecf163030f37e9407b4fd544b2146267a7d1
fgatto13 <128358663+fgatto13@users.noreply.github.com>
2023-07-03 16:50:45 +0000

    modifiche funzionali

--- TSWproject/src/main/webapp/js/animations.js
+++ TSWproject/src/main/webapp/js/animations.js
@@ -1,29 +1,31 @@
-function applyHoverEffect(divId) {
-  var $div = $('#' + divId);
+$(document).ready(function() {
+  function applyHoverEffect(divId) {
+    var $div = $('#' + divId);
 
-  $div.css({
-    transition: "transform 0.3s, box-shadow 0.3s",
-    opacity: "0.9"
-  });
-
-  $div.on("mouseover", function(event) {
-    $(this).css({
-      transform: "scale(1.05)",
-      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.3)",
-      opacity: "1"
-    });
-  });
-
-  $div.on("mouseout", function(event) {
-    $(this).css({
-      transform: "scale(1)",
-      boxShadow: "none",
+    $div.css({
+      transition: "transform 0.3s, box-shadow 0.3s",
       opacity: "0.9"
     });
-  });
-}
 
-// Call the function for elements with IDs from 1 to 5
-for (var i = 1; i <= 5; i++) {
-  applyHoverEffect(i);
-}
+    $div.on("mouseover", function(event) {
+      $(this).css({
+        transform: "scale(1.05)",
+        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.3)",
+        opacity: "1"
+      });
+    });
+
+    $div.on("mouseout", function(event) {
+      $(this).css({
+        transform: "scale(1)",
+        boxShadow: "none",
+        opacity: "0.9"
+      });
+    });
+  }
+
+  // Call the function for elements with IDs from 1 to 5
+  for (var i = 1; i <= 5; i++) {
+    applyHoverEffect(i);
+  }
+});
