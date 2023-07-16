995b90e082457bf154c90dbb84e201441c3cd0e8
fgatto13 <francescogatto2001@gmail.com>
2023-07-16 20:45:00 +0200

    bast

--- TSWproject/src/main/webapp/js/buttons.js
+++ TSWproject/src/main/webapp/js/buttons.js
@@ -4,6 +4,5 @@
 $(document).ready(function() {
 	$('#change-view').click(function() {
 		$('.table-row').toggleClass('change-view');
-		$('.image-block').toggleClass('change-imageBlock');
 	});
 });
